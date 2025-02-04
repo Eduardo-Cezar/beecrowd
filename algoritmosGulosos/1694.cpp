f27b473988ba9362be49d3f378f0856a9158113b
Eduardo-Cezar <edcezar442@gmail.com>
2025-02-04 19:22:47 -0300

    :heavy_check_mark: Aceito

--- algoritmosGulosos/1694.cpp
+++ algoritmosGulosos/1694.cpp
@@ -1,36 +1,62 @@
 #include <iostream>
 
-//#define FOR_UPTO(start, end) for (int it = start; it < end; it++)
-
 using namespace std;
 
-int main(){
+int main() {
     unsigned n, m, r, c;
-    int ** mat = nullptr;
+    int** mat = nullptr;
+    char pixel = ' ';
+    int operacoes = 0;
+    bool ehPosivel = true;
+
     n = m = r = c = 0;
 
-    cin >> n >> m >> r >> c;
+    while (cin >> n >> m >> r >> c, n || m || r || c) {
+        operacoes = 0;
+        bool ehPosivel = true;
 
-    mat = new int*[n];
-    for (int i = 0; i < n; i++){
-        mat[i] = new int[m];
-    }
+        mat = new int*[n];
+        for (int i = 0; i < n; i++)
+            mat[i] = new int[m];
 
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cin >> mat[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> pixel;
+                mat[i][j] = pixel - '0'; // converte char para int
+            }
         }
-    }
 
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cout << mat[i][j] << ' ';
+        // escolha gulosa
+        for (int i = 0; i <= n - r; i++) {
+            for (int j = 0; j <= m - c; j++) {
+                if (mat[i][j] == 1) {
+                    for (int x = i; x < i + r; x++) {
+                        for (int y = j; y < j + c; y++) {
+                            mat[x][y] = 1 - mat[x][y];
+                        }
+                    }
+                    operacoes++;
+                }
+            }
         }
-        cout << endl;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 1) {
+                    ehPosivel = false;
+                    break;
+                }
+            }
+            if (!ehPosivel) break;
+        }
+
+        cout << (ehPosivel ? operacoes : -1) << endl;
+
+        for (int i = 0; i < n; i++)
+            delete[] mat[i];
+
+        delete[] mat;
     }
 
-
-    delete [] mat;
     return 0;
 }
